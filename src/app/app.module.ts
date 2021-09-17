02721b3c8bc90eb3fbb7966e1b1b1b69f2e1b7cb
deepvn98 <deepvn988@gmail.com>
2021-09-17 15:36:42 +0700

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+
+
+const appRouter : Routes = [
+  {path:"",component:HomeComponent}
+ 
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
